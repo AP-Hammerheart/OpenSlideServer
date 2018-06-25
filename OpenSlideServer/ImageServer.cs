5182fc204f0132163f04e59133318f074ef8945e
ebragge <ebragge@live.com>
2018-06-25 11:19:20 +0300

    Initial commit

---
+++ OpenSlideServer/ImageServer.cs
@@ -0,0 +1,289 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace OpenSlideServer
+{
+    public class Settings
+    {
+        public string location;
+        public string cache;
+        public string[] prefixes;
+    }
+
+    public class ImageServer
+    {
+        public ImageServer()
+        {          
+            var listener = new HttpListener();
+            var settings = LoadSettings();
+
+            foreach (var prefix in settings.prefixes)
+            {
+                listener.Prefixes.Add(prefix);
+            }
+
+            listener.Start();
+
+            Console.WriteLine("Server running ...");
+
+            while (true)
+            {
+                try
+                {
+                    var context = listener.GetContext();
+                    ThreadPool.QueueUserWorkItem(o => HandleRequest(context, settings.location, settings.cache));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public Settings LoadSettings()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                var json = sr.ReadToEnd();
+                return JsonConvert.DeserializeObject<Settings>(json);
+            }
+        }
+
+        private void HandleRequest(object state, string location, string cache)
+        { 
+            var context = (HttpListenerContext)state;
+            try
+            {
+                var command = context.Request.QueryString.Get("command");
+                if (command == "image")
+                {
+                    HandleImageRequest(context, location, cache);
+                }
+                else if (command == "list")
+                {
+                    HandleListRequest(context, location);
+                }
+                else if (command == "cases")
+                {
+                    HandleCasesRequest(context, location);
+                }
+                else if (command == "details")
+                {
+                    HandleDetailsRequest(context, location);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+
+        private void HandleCasesRequest(HttpListenerContext context, string location)
+        {
+            try
+            {
+                Console.WriteLine(context.Request.RawUrl);
+
+                var dirs = Directory.GetDirectories(location);
+                var json = "{\r\n\t\"Cases\":[\r\n";
+
+                for (var i = 0; i < dirs.Length; i++)
+                {
+                    json += "\t\t\"" + Path.GetFileName(dirs[i]) + "\"" + ((i < dirs.Length - 1) ? ", \r\n" : "");
+                }
+                json += "\r\n\t]\r\n}";
+
+                var bytes = Encoding.ASCII.GetBytes(json);
+
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "application/json";
+
+                context.Response.ContentLength64 = bytes.Length;
+                context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Close();
+            }
+        }
+
+        private void HandleListRequest(HttpListenerContext context, string location)
+        {
+            try
+            {
+                Console.WriteLine(context.Request.RawUrl);
+
+                var caseID = context.Request.QueryString.Get("caseID");
+                var files = Directory.GetFiles(location + "\\" + caseID + "\\");
+                var json = "{\r\n\t\"Images\":[\r\n";
+
+                for (var i = 0; i < files.Length; i++)
+                {
+                    json += "\t\t\"" + Path.GetFileName(files[i]) + "\"" + ((i < files.Length - 1) ? ", \r\n" : "");
+                }
+                json += "\r\n\t]\r\n}";
+
+                var bytes = Encoding.ASCII.GetBytes(json);
+
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "application/json";
+
+                context.Response.ContentLength64 = bytes.Length;
+                context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Close();
+            }
+        }
+
+        private void HandleDetailsRequest(HttpListenerContext context, string location)
+        {
+            try
+            {
+                Console.WriteLine(context.Request.RawUrl);
+
+                var name = context.Request.QueryString.Get("name");
+                var caseID = context.Request.QueryString.Get("caseID");
+                var bytes = Utilities.ImageDetails(location + "\\" + caseID + "\\" + name);
+
+                if (bytes == null)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    context.Response.Close();
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    context.Response.ContentType = "application/json";
+
+                    context.Response.ContentLength64 = bytes.Length;
+                    context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Close();
+            }
+        }
+
+        private void HandleImageRequest(HttpListenerContext context, string location, string cache)
+        {
+            try
+            {
+                Console.WriteLine(context.Request.RawUrl);
+
+                var name = context.Request.QueryString.Get("name");
+                var caseID = context.Request.QueryString.Get("caseID");
+                var level = Int32.Parse(context.Request.QueryString.Get("level"));
+                var format = context.Request.QueryString.Get("format");
+
+                var x = Int64.Parse(context.Request.QueryString.Get("x"));
+                var y = Int64.Parse(context.Request.QueryString.Get("y"));
+                var w = Int32.Parse(context.Request.QueryString.Get("w"));
+                var h = Int32.Parse(context.Request.QueryString.Get("h"));
+
+                Console.WriteLine("Image:" + name
+                        + ", level:" + level.ToString()
+                        + ", X:" + x.ToString() + ", Y:" + y.ToString()
+                        + ", W:" + w.ToString() + ", H:" + h.ToString());
+
+                if (format == null)
+                {
+                    format = "PNG";
+                }
+
+                Console.WriteLine("Format: " + format);
+
+                if (w < 0 || h < 0 || w > 10000 || h > 10000 || level < 0)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    context.Response.Close();
+                }
+                else
+                {
+                    var start = DateTime.Now;
+                    Console.WriteLine("Start:" + start.ToString(" h:m:s.ff"));
+
+                    var fileName = name
+                            + "&x=" + x.ToString()
+                            + "&y=" + y.ToString()
+                            + "&w=" + w.ToString()
+                            + "&h=" + h.ToString()
+                            + "&level=" + level.ToString();
+
+                    var f = (cache + fileName + "." + format).ToLower();
+
+                    var bytes = Utilities.LoadFile(f);
+
+                    if (bytes == null)
+                    {
+                        bytes = Utilities.CreateRegion(location + "\\" + caseID + "\\" + name, level, x, y, w, h, format);
+                        if (bytes != null)
+                        {
+                            Console.WriteLine("File: " + f);
+                            Utilities.SaveFile(f, bytes);
+                        }
+                    }
+                           
+                    var end = DateTime.Now;
+
+                    Console.WriteLine("Time:" + (end - start).TotalMilliseconds.ToString() + " ms");
+
+                    if (bytes == null)
+                    {
+                        Console.WriteLine("bytes == null");
+
+                        context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                        context.Response.Close();
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+                        if (format == null || format.Equals("PNG"))
+                        {
+                            context.Response.ContentType = "image/png";
+                        }
+                        else if (format.Equals("RAW"))
+                        {
+                            context.Response.ContentType = "application/octet-stream";
+                        }
+                        else if (format.Equals("JPG"))
+                        {
+                            context.Response.ContentType = "image/jpeg";
+                        }
+                        else if (format.Equals("BMP"))
+                        {
+                            context.Response.ContentType = "image/bmp";
+                        }
+
+                        context.Response.ContentLength64 = bytes.Length;
+                        context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.Close();
+            }
+        }
+    }
+}
---
+++ OpenSlideServer/Utilities.cs
@@ -0,0 +1,179 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using SharpDX.WIC;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace OpenSlideServer
+{
+    class Utilities
+    {
+        public static byte[] ImageDetails(string name)
+        {
+            var osr = OpenSlideInterface.Openslide_open(name);
+
+            if (osr == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            var vendor = Marshal.PtrToStringAnsi(OpenSlideInterface.Openslide_detect_vendor(name));
+            var levels = OpenSlideInterface.Openslide_get_level_count(osr);
+
+            var widths = new Int64[levels];
+            var heights = new Int64[levels];
+            var dimensions = "[\r\n\t\t";
+
+            var offset = LoadText(name + ".txt");
+
+            if (offset == null)
+            {
+                offset = "\"0,0\"";
+            }
+
+            unsafe
+            {
+                Int64 w, h;
+                Int64* pw = &w;
+                Int64* ph = &h;
+
+                for (var l = 0; l < levels; l++)
+                {
+                    OpenSlideInterface.Openslide_get_level_dimensions(osr, l, pw, ph);
+                    widths[l] = w;
+                    heights[l] = h;
+                    dimensions += "\"" + l.ToString() + "," 
+                        + w.ToString() + "," 
+                        + h.ToString() + "\"" 
+                        + ((l < levels - 1) ? ",\r\n\t\t" : "");
+                }
+            }
+            dimensions += "\r\n\t]";
+
+            OpenSlideInterface.Openslide_close(osr);
+
+            var json = "{\r\n\t\"Name\":\"" + Path.GetFileName(name)
+                + "\", \r\n\t\"Vendor\":\"" + vendor
+                + "\", \r\n\t\"Levels\":" + levels.ToString()
+                + ", \r\n\t\"Width\":" + widths[0].ToString()
+                + ", \r\n\t\"Height\":" + heights[0].ToString()
+                + ", \r\n\t\"Dimensions\":" + dimensions
+                + ", \r\n\t\"Offset\":" + offset + "\r\n}";
+
+            return Encoding.ASCII.GetBytes(json);
+        }
+
+        public static byte[] CreateRegion(string name, Int32 level, Int64 x, Int64 y, Int32 w, Int32 h, string format)
+        {
+            var osr = OpenSlideInterface.Openslide_open(name);
+
+            if (osr == IntPtr.Zero)
+            {
+                Console.WriteLine("osr == IntPtr.Zero");
+                return null;
+            }
+
+            var buffer = Marshal.AllocHGlobal(4 * w * h);
+
+            OpenSlideInterface.Openslide_read_region(osr, buffer, x, y, level, w, h);
+            OpenSlideInterface.Openslide_close(osr);
+
+            byte[] bytes = null;
+
+            if (format == null || format.Equals("PNG"))
+            {
+                bytes = CreateFormat(buffer, w, h, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            else if (format.Equals("RAW"))
+            {
+                bytes = CreateRaw(buffer, w, h);
+            }
+            else if (format.Equals("JPG"))
+            {
+                bytes = CreateFormat(buffer, w, h, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            else if (format.Equals("BMP"))
+            {
+                bytes = CreateFormat(buffer, w, h, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+
+            Marshal.FreeHGlobal(buffer);
+
+            return bytes;
+        }
+
+        static byte[] CreateFormat(IntPtr pixels, int width, int height, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (var bitmap = new System.Drawing.Bitmap(width, height, width * 4, System.Drawing.Imaging.PixelFormat.Format32bppArgb, pixels))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, format);
+                    return stream.ToArray();
+                }
+            }           
+        }
+
+        static byte[] CreateRaw(IntPtr pixels, int width, int height)
+        {
+            using (var bitmap = new System.Drawing.Bitmap(width, height, width * 4, System.Drawing.Imaging.PixelFormat.Format32bppArgb, pixels))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                    var factory = new ImagingFactory2();
+                    using (var decoder = new BitmapDecoder(factory, stream, DecodeOptions.CacheOnDemand))
+                    {
+                        using (var formatConverter = new FormatConverter(factory))
+                        {
+                            formatConverter.Initialize(decoder.GetFrame(0), PixelFormat.Format32bppPRGBA);
+
+                            var stride = formatConverter.Size.Width * 4;
+                            using (var dataStream = new SharpDX.DataStream(formatConverter.Size.Height * stride, true, true))
+                            {
+                                formatConverter.CopyPixels(stride, dataStream);
+
+                                byte[] b;
+
+                                using (BinaryReader br = new BinaryReader(dataStream))
+                                {
+                                    b = br.ReadBytes((int)dataStream.Length);
+                                }
+
+                                return b;
+                            }                                                    
+                        }
+                    }
+                }
+            }
+        }
+
+        public static byte[] LoadFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return File.ReadAllBytes(fileName);
+            }
+            return null;
+        }
+
+        public static string LoadText(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                return File.ReadAllText(fileName);
+            }
+            return null;
+        }
+
+        public static void SaveFile(string fileName, byte[] data)
+        {
+            File.WriteAllBytes(fileName, data);
+        }
+    }
+}
